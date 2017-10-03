d49b8d6b517bc6c923696c3acf2ee126b2578cbd
edinchev <eric.dinchev@gmail.com>
2017-10-02 23:36:56 -0400

    Scrapper file working. Scraped files added.

--- src/Emulation/DespairScraper.js
+++ src/Emulation/DespairScraper.js
@@ -1,25 +1,65 @@
 var webdriverio = require('webdriverio');
+var fs = require('fs');
 
 var options = {
   desiredCapabilities: {
     browserName: 'chrome',
   },
-  host: 'selenium-chrome.localhost',
-  screenshotPath: './Screenshots',
   logOutput: './Logs',
   logLevel: 'verbose',
-  debug: true,
-  connectionRetryCount: 1
 };
 
-var url = 'https://despair.com/collections/demotivators';
+//@todo Figure out how to make this work dynamically.
+var url = 'https://despair.com/collections/demotivators?page=';
 
-var client = webdriverio.remote(options);
+webdriverio.remote(options)
+           .init()
+           .url(url + '1')
+           .getUrl()
+           .then(function(url) {
+             console.log('Parsing URL: ' + url);
+           })
+           .getText('.info .title')
+           .then(function(titles) {
+             createFile('titles-1.json', titles);
+           })
+           .getText('.info p')
+           .then(function(descriptions) {
+             createFile('descriptions-1.json',
+                 descriptions);
+           })
+           .url(url + '2')
+           .getUrl()
+           .then(function(url) {
+             console.log('Parsing URL: ' + url);
+           })
+           .getText('.info .title')
+           .then(function(titles) {
+             createFile('titles-2.json', titles);
+           })
+           .getText('.info p')
+           .then(function(descriptions) {
+             createFile('descriptions-2.json',
+                 descriptions);
+           })
+           .end();
 
-client.init()
-      .url('http://google.com')
-      .getTitle()
-      .then(function(title) {
-        console.log('Title was: ' + title);
-      })
-      .end();
+/**
+ *
+ * @param fileName
+ * @param data
+ */
+function createFile(fileName, data) {
+  console.log('Attempting to create file:' + fileName);
+
+  var folder = './Data';
+
+  fs.writeFile(folder + '/' + fileName, JSON.stringify(data),
+      function(error) {
+        if (error) {
+          console.log('Error trying to create ' + fileName + ':' +
+              error);
+        }
+        console.log(fileName + ' created.');
+      });
+}
