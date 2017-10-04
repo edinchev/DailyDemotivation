0d14dd4d5a24b209840f170b7a25ae35797b6691
edinchev <eric.dinchev@gmail.com>
2017-10-03 22:57:18 -0400

    DespairScraper PHP started. Composer autoloader added.

--- src/Emulation/PHP/DespairScraper.php
+++ src/Emulation/PHP/DespairScraper.php
@@ -1,2 +1,53 @@
 <?php
 
+namespace Emulation\PHP;
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+require_once('../../../vendor/autoload.php');
+
+class DespairScraper
+{
+    const SITE_URL = "https://despair.com/collections/demotivators";
+
+    private $seleniumHost = 'http://localhost:4444/wd/hub';
+
+    /** @var  RemoteWebDriver $driver */
+    private $driver;
+
+    private $titles = [];
+    private $descriptions = [];
+    private $numPages = 2;
+
+    public function __construct()
+    {
+        $capabilities = DesiredCapabilities::chrome();
+        $this->driver = RemoteWebDriver::create($this->seleniumHost, $capabilities, 5000);
+        $this->scrapeSite();
+        $this->driver->quit();
+    }
+
+    /**
+     *
+     */
+    private function scrapeSite()
+    {
+        for ($pageNum = 1; $pageNum <= $this->numPages; $pageNum++) {
+            $queryString = "?page={$pageNum}";
+            $fullUrl = DespairScraper::SITE_URL . $queryString;
+            $this->printMessage("Getting URL: {$fullUrl}");
+            $this->driver->get($fullUrl);
+            $this->driver->wait(10)->until();
+            $titles = $this->driver->findElement(WebDriverBy::className('.title'));
+            var_dump($titles);
+        }
+    }
+
+    private function printMessage(string $message)
+    {
+        echo("{$message}\n");
+    }
+}
+
+new DespairScraper();
