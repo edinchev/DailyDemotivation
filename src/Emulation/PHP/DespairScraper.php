bb26c93ed7f7b53606b7c90ff355e1cc01182ec9
edinchev <eric.dinchev@gmail.com>
2017-10-03 23:58:38 -0400

    PHP Scraper finished and working.

--- src/Emulation/PHP/DespairScraper.php
+++ src/Emulation/PHP/DespairScraper.php
@@ -2,14 +2,22 @@
 
 namespace Emulation\PHP;
 
+use Exception;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 
 require_once('../../../vendor/autoload.php');
 
 class DespairScraper
 {
+    private $debug = false;
+
     const SITE_URL = "https://despair.com/collections/demotivators";
+    const ELEMENT_WAIT_TIMEOUT = 5; // seconds
+    const ELEMENT_DESCRIPTION = '.info p';
+    const ELEMENT_TITLE = '.info .title';
 
     private $seleniumHost = 'http://localhost:4444/wd/hub';
 
@@ -18,6 +26,8 @@ class DespairScraper
 
     private $titles = [];
     private $descriptions = [];
+    private $titlesAndDescriptions = [];
+
     private $numPages = 2;
 
     public function __construct()
@@ -33,14 +43,92 @@ class DespairScraper
      */
     private function scrapeSite()
     {
+        // Loop through all pages and push titles and descriptions to properties.
         for ($pageNum = 1; $pageNum <= $this->numPages; $pageNum++) {
             $queryString = "?page={$pageNum}";
-            $fullUrl = DespairScraper::SITE_URL . $queryString;
+            $fullUrl = self::SITE_URL . $queryString;
+
             $this->printMessage("Getting URL: {$fullUrl}");
             $this->driver->get($fullUrl);
-            $this->driver->wait(10)->until();
-            $titles = $this->driver->findElement(WebDriverBy::className('.title'));
-            var_dump($titles);
+
+            $this->printMessage("Getting page titles.");
+            $this->getPageTitles();
+
+            $this->printMessage("Getting page descriptions.");
+            $this->getPageDescriptions();
+        }
+
+        $length = $this->validateTitleAndDescriptionLengths();
+        $this->combineTitlesAndDescriptions($length);
+
+        print_r($this->titlesAndDescriptions);
+        $this->printMessage("['" . implode("','", $this->titlesAndDescriptions) . "']");
+    }
+
+    private function validateTitleAndDescriptionLengths()
+    {
+        $this->printMessage('Validating Title and Description Lengths.');
+
+        $titlesLength = count($this->titles) - 1;
+        $descriptionsLength = count($this->descriptions) - 1;
+
+        if ($titlesLength != -1 && ($titlesLength != $descriptionsLength)) {
+            throw new Exception("Titles length of: {$titlesLength} does not match Descriptions length of: {$descriptionsLength}");
+        }
+
+        return $titlesLength;
+    }
+
+    /**
+     * @param $length
+     */
+    private function combineTitlesAndDescriptions($length)
+    {
+        $this->printMessage("Combining Titles and Descriptions");
+
+        for ($i = 0; $i <= $length; $i++) {
+            $this->titlesAndDescriptions[] = "{$this->titles[$i]}. {$this->descriptions[$i]}";
+        }
+    }
+
+    /**
+     * @return array
+     */
+    private function getPageTitles()
+    {
+        $this->driver->wait(self::ELEMENT_WAIT_TIMEOUT)
+            ->until(WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(
+                WebDriverBy::cssSelector(self::ELEMENT_TITLE)
+            ));
+        $elements = $this->driver->findElements(WebDriverBy::cssSelector(self::ELEMENT_TITLE));
+
+        foreach ($elements as $element) {
+            $this->titles[] = $element->getText();
+        }
+
+        if ($this->debug) {
+            var_dump($this->titles);
+        }
+    }
+
+    /**
+     *
+     */
+    private function getPageDescriptions()
+    {
+        $this->driver->wait(self::ELEMENT_WAIT_TIMEOUT)
+            ->until(WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(
+                WebDriverBy::cssSelector(self::ELEMENT_DESCRIPTION)
+            ));
+
+        $elements = $this->driver->findElements(WebDriverBy::cssSelector(self::ELEMENT_DESCRIPTION));
+
+        foreach ($elements as $element) {
+            $this->descriptions[] = addslashes($element->getText());
+        }
+
+        if ($this->debug) {
+            var_dump($this->descriptions);
         }
     }
 
